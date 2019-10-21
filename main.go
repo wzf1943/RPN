7dda3e64cb15ce0adbf500e52c2fa2835d3f6352
Joseph Wong <wzf2012@u.northwestern.edu>
2019-10-20 22:05:11 -0700

    Add parse RPN function

--- main.go
+++ main.go
@@ -4,14 +4,66 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/golang-collections/collections/stack"
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func evalRPN(tokens []string) (int, error) {
+	stk := stack.New()
+
+	for _, token := range tokens {
+		if token[0] == '-' && len(token) > 1 || token[0] >= '0' && token[0] <= '9' {
+			num, err := strconv.Atoi(token)
+			if err != nil {
+				return 0, fmt.Errorf("failed to cast token to int: %v", err)
+			}
+			stk.Push(num)
+			continue
+		}
+
+		right := stk.Pop()
+		left := stk.Pop()
+
+		if token == "+" {
+			stk.Push(left.(int) + right.(int))
+		}
+
+		if token == "-" {
+			stk.Push(left.(int) - right.(int))
+		}
+
+		if token == "*" {
+			stk.Push(left.(int) * right.(int))
+		}
+
+		if token == "/" {
+			stk.Push(left.(int) / right.(int))
+		}
+	}
+	log.Printf("reach here")
+	return stk.Peek().(int), nil
+}
+
+// RpnHandler get the reverse polish notaiton request from client
+func RpnHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("url inputs is %v", r.URL.Path[1:])
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	rpnstr := r.URL.Path[1:]
+	strs := strings.Split(rpnstr, ",")
+	log.Printf("%q\n", strs)
+	res, err := evalRPN(strs)
+	if err != nil {
+		log.Printf("erro: %v", err)
+		fmt.Fprintf(w, "error = %v", err)
+	}
+	log.Printf("res = %v", res)
+	fmt.Fprintf(w, "res = %v", res)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/{rpn}", RpnHandler)
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
